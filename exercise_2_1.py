a6ba85e340312b1c2ae12617099c55fe44ce2eb7
Mikołaj Przenzak <mprzenzak@it-dev.pl>
2022-11-07 20:10:17 +0100

    Generating 3D egg model

---
+++ exercise_2_1.py
@@ -0,0 +1,213 @@
+import math
+import random
+from datetime import time
+
+import numpy as np
+import numpy as numpy
+from glfw.GLFW import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import OpenGL.GLUT as GLUT
+
+
+def startup():
+    update_viewport(None, 400, 400)
+    glClearColor(0, 0, 0, 1.0)
+
+
+def update_viewport(window, width, height):
+    if width == 0:
+        width = 1
+    if height == 0:
+        height = 1
+    aspect_ratio = width / height
+
+    glMatrixMode(GL_PROJECTION)
+    glViewport(0, 0, width, height)
+    glLoadIdentity()
+
+    if width <= height:
+        glOrtho(-7.5, 7.5, -7.5 / aspect_ratio, 7.5 / aspect_ratio,
+                10.0, -10.0)
+    else:
+        glOrtho(-7.5 * aspect_ratio, 7.5 * aspect_ratio, -7.5, 7.5,
+                10.0, -10.0)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def axes():
+    x_min = (-5.0, 0.0, 0.0)
+    x_max = (5.0, 0.0, 0.0)
+
+    y_min = (0.0, -5.0, 0.0)
+    y_max = (0.0, 5.0, 0.0)
+
+    z_min = (0.0, 0.0, -5.0)
+    z_max = (0.0, 0.0, 5.0)
+
+    glColor3f(1.0, 0.0, 0.0)
+    glBegin(GL_LINES)
+    glVertex3fv(x_min)
+    glVertex3fv(x_max)
+    glEnd()
+
+    glColor3f(0.0, 1.0, 0.0)
+    glBegin(GL_LINES)
+
+    glVertex3fv(y_min)
+    glVertex3fv(y_max)
+    glEnd()
+
+    glColor3f(0.0, 0.0, 1.0)
+    glBegin(GL_LINES)
+
+    glVertex3fv(z_min)
+    glVertex3fv(z_max)
+    glEnd()
+
+
+def egg(size):
+    vertices_list = np.zeros((size, size, 3))
+    for i in range(size):
+        for j in range(size):
+            u = i / (size - 1)
+            v = j / (size - 1)
+            vertices_list[i][j][0] = (-90.0 * pow(u, 5.0) + 225.0 * pow(u, 4.0) - 270.0 *
+                                      pow(u, 3.0) + 180.0 * pow(u, 2.0) - 45.0 * u) * math.cos(math.pi * v)
+            vertices_list[i][j][1] = 160.0 * pow(u, 4.0) - 320.0 * pow(u, 3.0) + 160.0 * pow(u, 2.0) - 5
+            vertices_list[i][j][2] = (-90.0 * pow(u, 5.0) + 225.0 * pow(u, 4.0) - 270.0 * pow(u, 3.0) + 180.0 *
+                                      pow(u, 2.0) - 45.0 * u) * math.sin(math.pi * v)
+    # draw_points(vertices_list, size)
+    # draw_lines(vertices_list, size)
+    draw_triangles(vertices_list, generate_colors(size), size)
+
+
+def draw_points(vertices_tab, size):
+    glColor3f(1.0, 1.0, 1.0)
+
+    glBegin(GL_POINTS)
+    for i in range(size):
+        for j in range(size):
+            glVertex3fv(vertices_tab[i][j])
+    glEnd()
+
+
+def draw_lines(t, n):
+    glColor3f(0.0, 1.0, 0.0)
+    for w in range(1, n):
+        for k in range(n - 1):
+            lines_per_node(t, w, k)
+
+
+def lines_per_node(vertices_list, x, y):
+    glBegin(GL_LINES)
+
+    glVertex3fv(vertices_list[x][y])
+    glVertex3fv(vertices_list[x - 1][y])
+
+    glEnd()
+
+    glBegin(GL_LINES)
+
+    glVertex3fv(vertices_list[x][y])
+    glVertex3fv(vertices_list[x][y + 1])
+
+    glEnd()
+
+    glBegin(GL_LINES)
+
+    glVertex3fv(vertices_list[x][y])
+    glVertex3fv(vertices_list[x - 1][y + 1])
+
+    glEnd()
+
+
+def draw_triangles(nodes, colors, n):
+    for w in range(n):
+        for k in range(n - 1):
+            triangles_per_node(nodes, colors, w, k)
+
+
+def triangles_per_node(nodes, colors, w, k):
+    glBegin(GL_TRIANGLES)
+
+    glColor3f(colors[w][k][0], colors[w][k][1], colors[w][k][2])
+    glVertex3fv(nodes[w][k])
+    glColor3f(colors[w - 1][k][0], colors[w - 1][k][1], colors[w - 1][k][2])
+    glVertex3fv(nodes[w - 1][k])
+    glColor3f(colors[w - 1][k + 1][0], colors[w - 1][k + 1][1], colors[w - 1][k + 1][2])
+    glVertex3fv(nodes[w - 1][k + 1])
+
+    glEnd()
+
+    glBegin(GL_TRIANGLES)
+
+    glColor3f(colors[w][k][0], colors[w][k][1], colors[w][k][2])
+    glVertex3fv(nodes[w][k])
+    glColor3f(colors[w][k + 1][0], colors[w][k + 1][1], colors[w][k + 1][2])
+    glVertex3fv(nodes[w][k + 1])
+    glColor3f(colors[w - 1][k + 1][0], colors[w - 1][k + 1][1], colors[w - 1][k + 1][2])
+    glVertex3fv(nodes[w - 1][k + 1])
+
+    glEnd()
+
+
+def generate_colors(n):
+    tab = np.zeros((n, n, 3))
+    for i in range(n):
+        for j in range(n):
+            tab[i][j] = random.random()
+    return tab
+
+
+def spin(angle):
+    glRotatef(angle, 1.0, 0.0, 0.0)
+    glRotatef(angle, 0.0, 1.0, 0.0)
+    glRotatef(angle, 0.0, 0.0, 1.0)
+
+
+def render(size):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    glLoadIdentity()
+
+    axes()
+
+    spin(glfwGetTime() * 180 / 3.1415)
+
+    egg(size)
+
+    glFlush()
+
+
+def main():
+    print("Define egg size:")
+    size = int(input())
+
+    if not glfwInit():
+        sys.exit(-1)
+
+    window = glfwCreateWindow(400, 400, __file__, None, None)
+    if not window:
+        glfwTerminate()
+        sys.exit(-1)
+
+    glfwMakeContextCurrent(window)
+    glfwSetFramebufferSizeCallback(window, update_viewport)
+    glfwSwapInterval(1)
+
+    startup()
+    while not glfwWindowShouldClose(window):
+        render(size)
+        glfwSwapBuffers(window)
+        glfwPollEvents()
+
+    glfwTerminate()
+
+
+if __name__ == '__main__':
+    main()
--- exercise_4.py
+++ exercise_4.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python3
-import sys
 import random
 
 from glfw.GLFW import *
 
 from OpenGL.GL import *
-from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 
 def startup():
-    #update_viewport(400, 400)
+    update_viewport(None, 400, 400)
     glClearColor(0.5, 0.5, 0.5, 1.0)
 
 
@@ -20,67 +17,61 @@ def shutdown():
 
 def render(size, level, deformation):
     glClear(GL_COLOR_BUFFER_BIT)
-
     carpet(-size // 2, -size // 2, size, level, deformation)
     glFlush()
 
 
 def draw(x, y, size, deformation):
-    glColor3f(0.0, 1.0, 1.0)
-    glBegin(GL_TRIANGLES)
-    glVertex2f(0.0, 0.0)
-    glVertex2f(0.0, 50.0)
-    glVertex2f(50.0, 0.0)
-    glEnd()
-    # if deformation > 0:
-    #     glBegin(GL_POLYGON)
-    #
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x + random.uniform(0.0, deformation) - deformation / 2,
-    #                y + random.uniform(0.0, deformation) - deformation / 2)
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x + random.uniform(0.0, deformation) - deformation / 2,
-    #                y + size + random.uniform(0.0, deformation) - deformation / 2)
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x + size + random.uniform(0.0, deformation) - deformation / 2,
-    #                y + size + random.uniform(0.0, deformation) - deformation / 2)
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x + size + random.uniform(0.0, deformation) - deformation / 2,
-    #                y + random.uniform(0.0, deformation) - deformation / 2)
-    #
-    #     glEnd()
-    #
-    #
-    # else:
-    #     glBegin(GL_POLYGON)
-    #
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x, y)
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x, y + size)
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x + size, y + size)
-    #     glColor3f(random.random(), random.random(), random.random())
-    #     glVertex2f(x + size, y)
-    #
-    #     glEnd()
+    if deformation > 0:
+        glBegin(GL_POLYGON)
+
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x + random.uniform(0.0, deformation) - deformation / 2,
+                   y + random.uniform(0.0, deformation) - deformation / 2)
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x + random.uniform(0.0, deformation) - deformation / 2,
+                   y + size + random.uniform(0.0, deformation) - deformation / 2)
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x + size + random.uniform(0.0, deformation) - deformation / 2,
+                   y + size + random.uniform(0.0, deformation) - deformation / 2)
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x + size + random.uniform(0.0, deformation) - deformation / 2,
+                   y + random.uniform(0.0, deformation) - deformation / 2)
+
+        glEnd()
+        glFlush()
+
+
+    else:
+        glBegin(GL_POLYGON)
+
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x, y)
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x, y + size)
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x + size, y + size)
+        glColor3f(random.random(), random.random(), random.random())
+        glVertex2f(x + size, y)
+
+        glEnd()
+        glFlush()
 
 
 def carpet(x, y, size, level, deformation):
     size //= 3
-
-    for w in range(x + 3 * size):
-        for h in range(y + 3 * size):
-            if (h - y) != size or (w - x) != size:
+    for w in range(3):
+        for h in range(3):
+            if h != 1 or w != 1:
                 if level != 0:
-                    carpet(w, h, size, level - 1, deformation)
+                    carpet(x + w * size, y + h * size, size, level - 1, deformation)
                 else:
-                    draw(w, h, size, deformation)
+                    draw(x + w * size, y + h * size, size, deformation)
             h += size
         w += size
 
 
-def update_viewport(width, height):
+def update_viewport(window, width, height):
     if width == 0:
         width = 1
     if height == 0:
@@ -117,9 +108,6 @@ def main():
     if not window:
         glfwTerminate()
         sys.exit(-1)
-
-    # glutInitDisplayMode(GLUT_SINGLE)
-    # glutReshapeFunc(update_viewport)
 
     glfwMakeContextCurrent(window)
     glfwSetFramebufferSizeCallback(window, update_viewport)
